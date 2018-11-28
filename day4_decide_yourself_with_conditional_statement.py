b65b3be46ba4313cb4f6bf282a04c05c092a513b
saxena-rishabh <saxenarishabh100@gmail.com>
2018-11-28 19:33:21 +0530

    add day 4 challenge solution

---
+++ day4_decide_yourself_with_conditional_statement.py
@@ -0,0 +1,7 @@
+n = int(input())
+if n < 10:
+    print('I am happy as having no responsibilities.')
+elif n >= 10 and n < 18:
+    print('I am still happy but starts feeling pressure of life.')
+elif n >= 18:
+    print('I am very much happy as i handled the pressure very well.')
